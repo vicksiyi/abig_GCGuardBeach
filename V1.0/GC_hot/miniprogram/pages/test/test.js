b10c1336b9fc5a264f1a0acf760e18d75e85f469
ZeroIce <1724264854@qq.com>
2019-10-11 03:28:52 +0800

    增加 Wxparse

---
+++ V1.0/GC_hot/miniprogram/pages/test/test.js
@@ -0,0 +1,73 @@
+var WxParse = require('../../wxParse/wxParse.js');
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    article: ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    let _this = this
+    const db = wx.cloud.database()
+    db.collection('jk_content').get({
+      success(res) {
+        console.log(res.data[7].content)
+        WxParse.wxParse('article', 'html', res.data[10].content , _this, 5);
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
