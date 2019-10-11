ab656a44dfd44b99678b37890f2914b411a62dde
Ppersica <2249253741@qq.com>
2019-10-11 17:56:21 +0800

    添加新闻

--- V1.0/GC_hot/miniprogram/pages/news/news.js
+++ V1.0/GC_hot/miniprogram/pages/news/news.js
@@ -1,66 +1,38 @@
-// pages/news/news.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    current: 0,
+    hot: [
+      {
+        name: '浏览最多',
+        checked: false,
+        color: 'default'
+      },
+      {
+        name: '评论最多',
+        checked: false,
+        color: 'red'
+      },
+      {
+        name: '点赞最多',
+        checked: true,
+        color: 'blue'
+      },
+      {
+        name: '收藏最多',
+        checked: true,
+        color: 'green'
+      }
+    ]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleChange({ detail }) {
+    this.setData({
+      current: detail.key
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onChange(event) {
+    const detail = event.detail;
+    this.setData({
+      ['hot[' + event.detail.name + '].checked']: detail.checked
+    })
   }
 })
