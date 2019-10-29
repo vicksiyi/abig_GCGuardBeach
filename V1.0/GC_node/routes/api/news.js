27335616de6feb99c359032bd194cdd0fe11a702
GC怪兽 <3382065235@qq.com>
2019-10-29 10:44:20 +0800

    添加新闻UA

--- V1.0/GC_node/routes/api/news.js
+++ V1.0/GC_node/routes/api/news.js
@@ -4,6 +4,7 @@ const cheerio = require('cheerio')
 const router = express.Router();
 const passport = require('passport');
 const Entities = require('html-entities').XmlEntities;
+const randomUA = require('../../utils/userAgent').randomUA;
 const ErrorLog = require('../../models/ErrorLog')
 const New = require('../../models/New')
 
@@ -32,17 +33,24 @@ const clean_html = (html) => {
     }
 }
 
+router.get('/ceshi', (req, res) => {
+    console.log(randomUA())
+})
+
 // $routes /api/news/select/:num
 // @desc 推荐列表获取
 // @access public
 router.get('/select/:num', (req, res) => {
     let url = "http://search.sohu.com/search/meta"
-    let keyword = '海滩环境'
+    let keyword = req.body["keyword"]
     let num = req.params.num
     let spider_url = encodeURI(`${url}?keyword=${keyword}&terminalType=pc&spm-pre=smpc.content.search-box.1.1572234057599y7tr5RL&SUV=1908090048509410&searchType=news&from=${num}`);
     (async () => {
         try {
-            const request_data = await superagent.get(spider_url);
+            const request_data = await superagent.get(spider_url).set(
+                "User-Agent", randomUA()
+            );
+            console.log(request_data.req)
             let data = JSON.parse(request_data.text)
             res.json(data.data.news)
             // spider_page(data.data.news[1].url, req, res)
@@ -76,8 +84,8 @@ router.get('/addNews', passport.authenticate('jwt', { session: false }), (req, r
                         new_title: title,
                         new_time: time,
                         new_from: from,
-                        new_type: '热点',
-                        new_image: 'null',
+                        new_type: req.body["type"],
+                        new_image: req.body["image"],
                         new_content: entities.decode(content)
                     }).save().then(New => {
                         res.json(New)
