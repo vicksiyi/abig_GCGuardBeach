2e3d3440e74b030546c56a57513e8204e360d062
GC怪兽 <3382065235@qq.com>
2019-10-30 21:09:21 +0800

    修改keys&添加全览

--- V1.0/GC_node/config/keys.js
+++ V1.0/GC_node/config/keys.js
@@ -2,8 +2,8 @@ module.exports = {
     mongoURI: "mongodb://localhost/user",
     secretOrket: 'secret',
     secretUser: 'secretUser',
-    APPID: 'wxd13cd8f778c55aea',
-    SECRET: '8ba2e41d39c2f6060ce7a62a63b7d0bb',
-    env: 'koobling-f64e80',
+    APPID: 'wx564d2807694ef073',
+    SECRET: '3c3dcaf2225e5ab3669d5201fd72aa25',
+    env: 'kkworkspace-4sdw7',
     mini_token_url:`https://api.weixin.qq.com/cgi-bin/token`
 }
