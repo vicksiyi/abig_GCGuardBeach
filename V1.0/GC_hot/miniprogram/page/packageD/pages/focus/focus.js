6a4082ca5deb18c521012abc47e837fbf3e22fda
GC怪兽 <3382065235@qq.com>
2019-11-18 16:00:45 +0800

    关注圈样式

--- V1.0/GC_hot/miniprogram/page/packageD/pages/focus/focus.js
+++ V1.0/GC_hot/miniprogram/page/packageD/pages/focus/focus.js
@@ -5,13 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    height: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let _this = this
     wx.setNavigationBarTitle({ title: `${options.type}圈` })
+    wx.getSystemInfo({
+      success(res) {
+        //创建节点选择器
+        var query = wx.createSelectorQuery();
+        //选择id
+        query.select('#wrapper').boundingClientRect()
+        query.exec(function (re) {
+          _this.setData({
+            height: res.windowHeight - re[0].height
+          })
+        });
+      }
+    })
   }
 })
