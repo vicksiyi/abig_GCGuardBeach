4b6b2424ea424c5d362d29214ff7ff84412b2b9c
TW@CK <39822906+vicksiyi@users.noreply.github.com>
2022-02-09 09:59:47 +0800

    Update keys.js

--- V1.0/GC_node/miniApp/config/keys.js
+++ V1.0/GC_node/miniApp/config/keys.js
@@ -2,12 +2,12 @@ module.exports = {
     mongoURI: "mongodb://localhost/user",
     secretUser: '2225e5ab3669d5201fd72aa25',
     APPID: 'wx564d2807694ef073',
-    SECRET: '3c3dcaf2225e5ab3669d5201fd72aa25',
+    SECRET: 'xxxxxxxxxxxxxxxxxx',
     env: 'kkworkspace-4sdw7',
     mini_token_url: `https://api.weixin.qq.com/cgi-bin/token`,
     email_server: {
         user: "1724264854@qq.com",
-        password: "ddjlcgwicxytdjgf",
+        password: "xxxxxxxxxxxxxxxxxx",
         host: "smtp.qq.com",
         ssl: true
     }
